e78bb337b800c853d283b47a30a35ad0b8e72c9d
Ansoumane2000 <103797557+Ansoumane2000@users.noreply.github.com>
2022-11-16 23:58:40 +0100

    mon_site: sauvegarde 2

---
+++ script.js
@@ -0,0 +1,9 @@
+//Variable gloabale
+let compteur =0
+let timer,elements,slide
+window.onload=()=>{
+    const diapo =document.querySelector(".diapo")
+    elements = document.querySelector(".elements")
+    slide = Array.from(elements.children)
+     
+}
